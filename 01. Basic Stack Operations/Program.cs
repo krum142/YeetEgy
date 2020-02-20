ec5ac9b67eb7bb9acb766975bf3c7542c42fceca
Krum Arnaudov <26346604+krum142@users.noreply.github.com>
2020-02-20 16:03:16 +0200

    Test

---
+++ 01. Basic Stack Operations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise_stacks_and_queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+
+            var N = int.Parse(input[0]);
+            var S = int.Parse(input[1]);
+            var X = int.Parse(input[2]);
+
+            var numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+            for (int i = 0; i < S; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+
+            }
+        }
+    }
+}
